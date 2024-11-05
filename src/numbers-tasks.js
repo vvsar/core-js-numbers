e77bcefdb7705d938ae5a6c6827443b9bdf201ac
Victor Saraev <vvsar@mail.ru>
2024-11-06 02:34:08 +0300

    feat: add getSumToN solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -284,8 +284,9 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  if (n === 1) return 1;
+  return n + getSumToN(n - 1);
 }
 
 /**
