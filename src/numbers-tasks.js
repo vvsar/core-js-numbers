5c974ffc3fbf4a074464c36d0773410efec6d45c
Victor Saraev <vvsar@mail.ru>
2024-11-06 02:49:05 +0300

    feat: add getSumOfDigits solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -300,8 +300,13 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  const arr = num.toString().split('');
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += +arr[i];
+  }
+  return sum;
 }
 
 /**
