112ceada81719b8cbf8e5a774b92f872645faee9
Victor Saraev <vvsar@mail.ru>
2024-11-07 12:30:01 +0300

    feat: add getCountOfOddNumbers solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -632,8 +632,14 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  for (let i = 1; i <= Math.abs(number); i += 1) {
+    if (i % 2 !== 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
